fa93a40b7fb3dcdc2ce13e56b72d956d5073a48f
Akash-Git-Maker <akasharjun.davane@gmail.com>
2024-08-24 21:46:59 +0530

    updated

--- src/main/java/com/akashit/Application.java
+++ src/main/java/com/akashit/Application.java
@@ -1,8 +1,12 @@
 package com.akashit;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
 @SpringBootApplication
 public class Application {
 
